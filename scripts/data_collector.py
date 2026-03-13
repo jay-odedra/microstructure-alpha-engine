31e5b887cc95ef8f872ee8b8cf2b63419da65698
Jay Odedra <jayodedra1998@gmail.com>
2026-03-13 07:02:54 +0000

    fixed notebook definitions and create more variables

--- scripts/data_collector.py
+++ scripts/data_collector.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import numpy as np
 from binance.client import Client
@@ -15,22 +14,20 @@ depth = client.get_order_book(symbol="BTCUSDT")
 trades = client.get_recent_trades(symbol="BTCUSDT", limit=1000)
 
 
-
-
 depth_buffer = []
 trades_buffer = []
 last_trade_id = 0
 buffer_size = 500
 for i in tqdm(range(50000)):
     timestamp = int(time.time() * 1000)
-    
+
     t0 = int(time.time() * 1000)
 
     depth = client.get_order_book(symbol="BTCUSDT", limit=100)
-    
+
     t1 = int(time.time() * 1000)
-    latency = (t1-t0) / 2
-    
+    latency = (t1 - t0) / 2
+
     bids = np.array(depth["bids"], dtype=float)
     asks = np.array(depth["asks"], dtype=float)
     depth_buffer.append(
@@ -43,15 +40,21 @@ for i in tqdm(range(50000)):
         }
     )
     trades = client.get_recent_trades(symbol="BTCUSDT", limit=1000)
-    
-    trades = [trade for trade in trades if trade["id"]>last_trade_id]
+
+    trades = [trade for trade in trades if trade["id"] > last_trade_id]
     if len(trades) > 0:
         last_trade_id = trades[-1]["id"]
     trades_buffer.extend(trades)
-    
-    if len(depth_buffer) >= buffer_size:    
-        pd.DataFrame(depth_buffer).to_parquet(f"E:\\Quant_Projects\\microstructure-alpha-engine\\microstructure-alpha-engine\\data\\raw\\lob\\lob_data_raw_{i}.parquet",compression="snappy")
-        pd.DataFrame(trades_buffer).to_parquet(f"E:\\Quant_Projects\\microstructure-alpha-engine\\microstructure-alpha-engine\\data\\raw\\trades\\trades_data_raw_{i}.parquet",compression="snappy")
+
+    if len(depth_buffer) >= buffer_size:
+        pd.DataFrame(depth_buffer).to_parquet(
+            f"E:\\Quant_Projects\\microstructure-alpha-engine\\microstructure-alpha-engine\\data\\raw\\lob\\lob_data_raw_{i}.parquet",
+            compression="snappy",
+        )
+        pd.DataFrame(trades_buffer).to_parquet(
+            f"E:\\Quant_Projects\\microstructure-alpha-engine\\microstructure-alpha-engine\\data\\raw\\trades\\trades_data_raw_{i}.parquet",
+            compression="snappy",
+        )
         depth_buffer = []
         trades_buffer = []
     time.sleep(1)
